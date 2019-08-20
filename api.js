aa431eb076fd4431dcfa3f4fdbf8fe0b21e48bda
Eric <ejwinebrenner@gmail.com>
2019-08-20 09:13:07 -0400

    api project start 11/50

---
+++ api.js
@@ -0,0 +1,36 @@
+const baseURL = "https://api.edamam.com/search"; //1
+const key = '5037af3e4c770201729aa03275f921c8'	;
+const appId  = '9cdb89be';
+let url;
+
+const searchTerm = document.querySelector('.search');
+const searchForm = document.querySelector('form');  
+const submitBtn = document.querySelector('.submit');
+
+searchForm.addEventListener('submit', fetchResults);
+
+
+// Not pulling up results when I input a searchTerm.value.  An array is being pulled when I hardcode a value within the ?q=...
+    function fetchResults(e) {
+        // e.preventDefault();
+        console.log(e); 
+        // url = baseURL + "?q=potato" + '&app_id='+ appId + "&app_key=" + key;
+        url = baseURL + "?q=" + searchTerm.value + '&app_id='+ appId + "&app_key=" + key;
+        console.log(url);
+                
+    fetch(url)
+    .then(function(results){
+        return results.json();
+    }) .then(function(json){
+        console.log(json);
+        // displayResults(json);   
+    });
+
+}
+
+    // function fetchResults(e){
+    //     e.preventDefault();
+    //     url = baseURL + "?q=" + searchTerm.value + '&app_id='+ appId + "&app_key=" + key;
+    // }
+
+fetchResults();
