f0d7929f1a6545eb39449a1b154e5d0104c1083c
Eric <ejwinebrenner@gmail.com>
2019-08-20 13:35:36 -0400

    firebase deployment

--- api.js
+++ api.js
@@ -6,13 +6,15 @@ let url;
 const searchTerm = document.querySelector('.search');
 const searchForm = document.querySelector('form');  
 const submitBtn = document.querySelector('.submit');
+const setTable = document.querySelector('.onTable');
 
 searchForm.addEventListener('submit', fetchResults);
 
 
+
 // Not pulling up results when I input a searchTerm.value.  An array is being pulled when I hardcode a value within the ?q=...
     function fetchResults(e) {
-        // e.preventDefault();
+        e.preventDefault();
         console.log(e); 
         // url = baseURL + "?q=potato" + '&app_id='+ appId + "&app_key=" + key;
         url = baseURL + "?q=" + searchTerm.value + '&app_id='+ appId + "&app_key=" + key;
@@ -23,14 +25,15 @@ searchForm.addEventListener('submit', fetchResults);
         return results.json();
     }) .then(function(json){
         console.log(json);
-        // displayResults(json);   
+        // displayResults(json);
+        
+    let hungryFor = json.hits[0].recipe.label;
+    console.log(hungryFor);   
     });
 
+
+    let plate = document.getElementById('onTable');
+        plate.innerText = 'stuff being noted';
+        inputField.appendChild(plate);
+
 }
-
-    // function fetchResults(e){
-    //     e.preventDefault();
-    //     url = baseURL + "?q=" + searchTerm.value + '&app_id='+ appId + "&app_key=" + key;
-    // }
-
-fetchResults();
